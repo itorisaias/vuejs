3c36ee027225bcc49a2ea8f38d7a1cf9f982c4a1
Itor Isaias <itorsilva@saofrancisco.com.br>
2019-03-04 01:23:11 -0300

    add tag vuetify, path router for todo

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/dog">Dog</router-link>
+  <v-app>
+    <div id="app">
+      <div id="nav">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link> |
+        <router-link to="/dog">Dog</router-link> |
+        <router-link to="/todo">Todo</router-link>
+      </div>
+      <router-view/>
     </div>
-    <router-view/>
-  </div>
+  </v-app>
 </template>
 
 <style lang="scss">
